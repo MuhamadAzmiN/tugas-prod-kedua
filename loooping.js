45c4d31fca2fcb47bd380565356f59026b267f36
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2024-01-17 07:43:46 +0700

    tugas

---
+++ loooping.js
@@ -0,0 +1,10 @@
+// for 
+for(let i = 0;i<10;i++){
+    console.log(`hai${i}`)
+}
+
+
+do{
+    console.log("hai ")
+    
+}while()
---
+++ script.js
@@ -0,0 +1,73 @@
+// const karyawan = "Azmi";
+// const gajiPokok = 30000;
+// const tunjangan = 0.2 * gajiPokok;
+// const pajak = 0.15 * (gajiPokok + tunjangan);
+// const gajiBersih = gajiPokok + tunjangan - pajak;
+
+// console.log(`nama ${karyawan} gaji pokok nya adalah ${gajiPokok} tunjangan nya adalah ${tunjangan} dan pajak nya adalah ${pajak} lalu gajih bersih nya adalah ${gajiBersih}`);
+
+
+
+
+
+//algoritma jam ke menit 
+
+
+
+// const jam = 1;
+// const menit = 30;
+// const detik = 40;
+
+
+// const hasil = jam * 3600 + menit * 60 + detik;
+
+
+// console.log(`jadi detik ny adalah ${hasil}`);
+
+
+
+
+// no 3\
+
+
+let totalDetik = 5440;
+let jam = Math.floor(totalDetik / 3600)
+let sisaDetik = totalDetik % 3600;
+
+let menit = Math.floor(sisaDetik / 60);
+let detik = sisaDetik % 60 ;
+console.log(`${jam} jam ${menit} menit ${detik} detik`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+
+
+
+
+
+
+
+
+
+
