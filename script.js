f5f6924f74bee3c7f7e2a71bfe0f91ad75278bd5
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2024-01-18 12:55:55 +0700

    lkpd

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// const karyawan = "Azmi";
-// const gajiPokok = 30000;
-// const tunjangan = 0.2 * gajiPokok;
-// const pajak = 0.15 * (gajiPokok + tunjangan);
-// const gajiBersih = gajiPokok + tunjangan - pajak;
+const karyawan = "Azmi";
+const gajiPokok = 30000;
+const tunjangan = 0.2 * gajiPokok;
+const pajak = 0.15 * (gajiPokok + tunjangan);
+const gajiBersih = gajiPokok + tunjangan - pajak;
 
-// console.log(`nama ${karyawan} gaji pokok nya adalah ${gajiPokok} tunjangan nya adalah ${tunjangan} dan pajak nya adalah ${pajak} lalu gajih bersih nya adalah ${gajiBersih}`);
+console.log(`nama ${karyawan} gaji pokok nya adalah ${gajiPokok} tunjangan nya adalah ${tunjangan} dan pajak nya adalah ${pajak} lalu gajih bersih nya adalah ${gajiBersih}`);
 
 
 
@@ -12,17 +12,6 @@
 
 //algoritma jam ke menit 
 
-
-
-// const jam = 1;
-// const menit = 30;
-// const detik = 40;
-
-
-// const hasil = jam * 3600 + menit * 60 + detik;
-
-
-// console.log(`jadi detik ny adalah ${hasil}`);
 
 
 // no 3\
